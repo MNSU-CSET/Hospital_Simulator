18b12e929c43e5ad729585da4fab1754e697cd74
Jeremy Fischer <jeremyfischer52@gmail.com>
2021-12-19 14:59:29 -0600

    Added Position Functionality to Script and Scene

--- Assets/Scripts/UI/DebuggerUI.cs
+++ Assets/Scripts/UI/DebuggerUI.cs
@@ -9,7 +9,7 @@ public class DebuggerUI : MonoBehaviour
     // Update is called once per frame
     [SerializeField] private TextMeshProUGUI FpsText;
     [SerializeField] private TextMeshProUGUI PositionText;
-    [SerializeField] private XRRig xrRig;
+    [SerializeField] private GameObject cameraObject;
 
 
     private float pollingTime = 1f;
@@ -20,33 +20,39 @@ public class DebuggerUI : MonoBehaviour
     {
         time += Time.deltaTime;
         frameCount++;
+        PollingTimeActivation();
+    }
 
-        UpdateFPS();
-        UpdatePosistion();
+    // All methods that rely on pollingTime get called here.
+    private void PollingTimeActivation()
+    {
+        if (time >= pollingTime)
+        {
+            UpdateFPS();
+            UpdatePosistion();
+
+            time -= pollingTime;
+        }
     }
 
     private void UpdatePosistion()
     {
-        float z = xrRig.transform.position.z;
-        float x = xrRig.transform.position.x;
-        float y = xrRig.transform.position.y;
+        float z = cameraObject.transform.position.z;
+        float x = cameraObject.transform.position.x;
+        float y = cameraObject.transform.position.y;
 
-        if (time >= pollingTime)
-        {
-            PositionText.text = $"Position (X, Y, Z): {x}, {y}, {z}";
-        }
+        PositionText.text = $"Position (X, Y, Z): {x}, {y}, {z}";
+        Debug.Log($"Position (X, Y, Z): {x}, {y}, {z}");
+
     }
 
     // Tracks the time and frame count. Updates every pollingTime seconds to a Text Object
     private void UpdateFPS()
     {
-        if (time >= pollingTime)
-        {
-            int frameRate = Mathf.RoundToInt(frameCount / time);
-            FpsText.text = frameRate.ToString() + " FPS";
 
-            time -= pollingTime;
-            frameCount = 0;
-        }
+        int frameRate = Mathf.RoundToInt(frameCount / time);
+        FpsText.text = frameRate.ToString() + " FPS";
+
+        frameCount = 0;
     }
 }
