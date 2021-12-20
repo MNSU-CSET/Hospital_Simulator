41a85feb2b29654bfc373a009da8bcf6e5c33010
Jeremy Fischer <jeremyfischer52@gmail.com>
2021-12-20 16:14:49 -0600

    Add Clean Attrivbute to hand. Put hand under blue hand sanitizer to become "Clean"

---
+++ Assets/Scripts/Rig and Character/Hand.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    // Hands can either be clean, or not.
+    [SerializeField] private bool isClean = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Cleaning Object")
+        {
+            isClean = true;
+        }
+    }
+}
