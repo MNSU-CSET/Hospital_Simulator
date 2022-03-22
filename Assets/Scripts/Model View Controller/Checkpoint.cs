bdbb0dbc835a452f3096ffa436b8d5744c9fcdcf
natebursch <92119395+natebursch@users.noreply.github.com>
2022-03-21 20:33:38 -0500

    Update Checkpoint.cs

--- Assets/Scripts/Model View Controller/Checkpoint.cs
+++ Assets/Scripts/Model View Controller/Checkpoint.cs
@@ -7,4 +7,6 @@ public class Checkpoint : MonoBehaviour
     //checkpoint will be a list of tasks
     public bool CheckpointComplete = false;
     public Task[] tasksInCheckpoint;
+
+    //this is where we will get a list of checkpoints
 }
